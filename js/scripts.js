4e69c3c45770d7779aadc2d49e2bab3c21eaab66
Pradeep B G <Pradeep_BG@SLP160114-Pradeepbg.local>
2018-10-29 23:22:04 +0530

    here i'm adding the full page sample

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$('#fullpage').fullpage({
+		//sectionsColor: ['#0F2980', '#ffd200', '#c1c1c1'],
+		anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourSection', 'fiveSection', 'sixSection',
+		'sevenSection', 'eightSection'],
+		menu: '.main-nav ul',
+		licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+		slidesNavigation: true,
+		fadingEffect: true,
+		
+	});
+	
+});
